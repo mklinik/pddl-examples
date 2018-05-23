42279853c594a53dcae6cf7b5a486977f7765247
Markus Klinik <mkl@lambdanaut.net>
2018-05-23 10:39:45 +0200

    comments

--- plan.sh
+++ plan.sh
@@ -9,20 +9,23 @@ fi
 DOMAINFILE="$1"
 PROBFILE="$2"
 
-# use fast-downward to make a plan
+# Use fast-downward to make a plan.
+# The resulting plan is in sas_plan.
 fast-downward.py "$DOMAINFILE" "$PROBFILE" --search "astar(blind())"
 
-# remove the last line in sas_plan
+# Remove the last line in sas_plan. Required for lifter.py, according to
+# private conversation with the author.
 sed -i '$d' sas_plan
 
-# run the KK algorithm to get a plan deordering 
+# Run the KK algorithm to get a plan deordering.
 python ~/radboud/src/planningAndScheduling/pop-gen/lifter.py \
     -domain "$DOMAINFILE" \
     -prob "$PROBFILE" \
     -mercout sas_plan DOT > result.dot
 
-# only take the DOT output of lifter
+# lifter.py outputs debugging info and the result data on stdout. Only take
+# the DOT output of lifter
 sed -ni '/digraph/,$p' result.dot
 
-# make svg image from dot file
+# Make svg image from dot file.
 dot -Tsvg result.dot > result.svg
